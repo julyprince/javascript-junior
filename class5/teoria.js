f81a7cf88b2f1da43fd3888d82428f0320d3e923
julyprince <princejec8080@gmail.com>
2022-10-07 10:24:11 -0400

    solved problems with console

---
+++ class5/teoria.js
@@ -0,0 +1,17 @@
+
+/***funciones de registro */
+console.assert(5>3); //falso si la condicion es falsa
+
+console.clear() //borra la consola
+console.table([1,3,4,5]) //muestra una tabla con indice valor
+console.warn("warnning") // muestra warning
+console.dir([1,3,4,5])// muestra el array
+console.count() // cuenta la ejecuciion de la funcion
+console.count() // cuenta la ejecuciion de la funcion
+
+console.group() // crea un grupos de consola
+console.groupEnd()//elimna o cierra el grupo
+
+console.time()// inicia tiempo de ejecucion
+console.timeLog() //cuenta cantidad de tiempo de ejecucion
+console.timeEnd() // finaliza el tiempo de ejecucion
